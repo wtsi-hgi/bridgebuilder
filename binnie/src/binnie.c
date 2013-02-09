a8105bc8cad17843a7608d032997b529ce58f499
Joshua C. Randall <jcrandall@alum.mit.edu>
2013-02-09 22:00:45 +0000

    Added copyright notice and license to binnie.c as well as the required LICENSE text and note in README.md

--- binnie/src/binnie.c
+++ binnie/src/binnie.c
@@ -1,3 +1,25 @@
+/*
+ * binnie - processes original and bridge remapped BAM into four "bins"
+ *
+ * Copyright (c) 2013 Genome Research Ltd. 
+ * Author: Joshua C. Randall <jcrandall@alum.mit.edu>
+ *
+ * This file is part of BridgeBuilder. 
+ *
+ * BridgeBuilder is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation; either version 3 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
