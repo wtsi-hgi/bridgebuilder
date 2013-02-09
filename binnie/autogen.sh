e94ea505cc14543d727142115a8368b70991470f
Joshua Randall <jcrandall@alum.mit.edu>
2013-02-09 15:59:03 +0000

    Don't use bash extensions to sh and to not unnecessarily call autoreconf (bootstrap does it already).
    Fixed version requirements for automake and autoconf and moved deprecated functionality from Makefile.am to configure.ac

--- binnie/autogen.sh
+++ binnie/autogen.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [[ ! -d gnulib ]]; then 
-    ./bootstrap
+if [ ! -d ./gnulib ] ; then 
+    ./bootstrap || (echo "bootstrap failed" && rm -rf ./gnulib && exit 1)
+else
+    autoreconf --verbose --install || (echo "autoreconf failed" && exit 1)
 fi
 
-autoreconf -v --install || exit 1
