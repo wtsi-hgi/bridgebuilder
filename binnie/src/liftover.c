cbb3aab27de99e59bf1dcc1797ab6c678d6bdc95
Nicholas Clarke <nc6@sanger.ac.uk>
2013-12-11 12:39:11 +0000

    Add license file, some documentation.

--- binnie/src/liftover.c
+++ binnie/src/liftover.c
@@ -1,3 +1,28 @@
+/*
+ * liftover.c Binnie co-ordinate liftover.
+ *
+ * Copyright (c) 2013 Genome Research Ltd. 
+ * Author: Nicholas Clarke <nicholas.clarke@sanger.ac.uk>
+ *
+ * This file is part of BridgeBuilder. 
+ *
+ * BridgeBuilder is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation; either version 3 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ /*
+  * Simple tool to liftover co-ordinates. At the moment, takes in data of the form
+  * chr\tposition and a liftover file and outputs the resulting chromosone and position.
+ */
 #include "binnie_coordmap.h"
 #include "config.h"
 #include <stdlib.h>
@@ -18,7 +43,7 @@ Range* createRange(char* input) {
     new->start = from_pos-1;
     new->end = from_pos-1;
     char *prefix_sn = xmalloc(LINE_LENGTH * sizeof(char));
-    prefix_sn = strncat(prefix_sn, "chr", 4);
+    // prefix_sn = strncat(prefix_sn, "chr", 4);
     new->id = strncat(prefix_sn, from_sn, LINE_LENGTH);
     free(from_sn);
     return new;
@@ -73,7 +98,6 @@ int main(int argc, int *argv[])
       fprintf(out, ".\t.\n");
     }
   }
-  
 
   bc_free_coordmap(map);
 
